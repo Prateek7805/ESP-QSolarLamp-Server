aa01eaf9dd71333c0e5bc1d2b68090947ab982db
Prateek7805 <prateekpjimmy@gmail.com>
2023-07-02 16:11:14 +0530

    Added Register

--- Routes/Device_Route.js
+++ Routes/Device_Route.js
@@ -6,7 +6,6 @@ const cookie_parser = require('cookie-parser');
 
 const user_auth = require('../Authentication/User_Auth');
 const dm_user = require('../DBO/Central_User_Device_Sch');
-const dm_license_key = require('../DBO/License_Key_Sch');
 const device_joi = require('../Validation/Device_Joi');
 const device_name_valid = require('../Validation/Device_Name');
 const pass_valid = require('../Validation/Password');
@@ -51,8 +50,39 @@ router.post('/register', async (req, res)=>{
             const {code, message} = license_key_check;
             return res.status(code).json({message});
         }
-        
-        
+        const license_key_db = license_key_check.message;
+
+        const user = await dm_user.findById({_id: user_id});
+
+        if(!user){
+            return res.status(404).json({message: 'user not found'});
+        }
+
+        if(user.devices.some(device=>device.name === name)){
+            return res.status(409).json({message: 'device name already exists'});
+        }
+        const hashed_password = await bcrypt.hash(password, 10);
+        //add refresh token for mcu here
+        const new_device = {
+            name: name,
+            password: hashed_password,
+            status: {
+                power : false,
+                brightness: 30
+            }
+        };
+        user.devices.push(new_device);
+        await user.save();
+
+        const device_id = user.devices.find(device=>device.name === name)._id;
+        if(!device_id){
+            return res.status(500).json({message: `device with name ${name} could not be created`});
+        }
+
+        license_key_db.device_id = device_id;
+        await license_key_db.save();
+
+        return res.status(200).json({message: 'Device successfully registered'});
 
     }catch(err){
         return res.status(500).json({message: err});
