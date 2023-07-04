364de1d78c00e07f18d964c4a71625b94551486a
Keralkar <sheela.keralkar@hp.com>
2023-07-04 23:24:43 +0530

    Created the API to verify the user.

--- Common/API_Urls.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const API_Urls = {
-    VerifyEmailURL: process.env.APPLICATION_END_POINT + 'verify_user?id='
-}
-
-module.exports = {
-    API_Urls 
-}
---
+++ Common_Utils/API_Urls.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const API_Urls = {
+    VerifyEmailURL: process.env.APPLICATION_END_POINT + 'verify?id='
+}
+
+module.exports = API_Urls 
