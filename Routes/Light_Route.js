c5d2e6970512d50f455584dfd7b5dfd342575fc9
Prateek7805 <prateekpjimmy@gmail.com>
2023-08-11 12:20:43 +0530

    Optimization in light login

--- Routes/Light_Route.js
+++ Routes/Light_Route.js
@@ -57,7 +57,7 @@ router.post('/login', async (req, res) => {
             return res.status(401).json({ message: "Incorrect Password" });
         }
         const token = light_auth.get_token(device_id);
-        return res.status(200).json({ token });
+        return res.status(200).send(token);
     } catch (err) {
         console.log(err);
         return res.status(500).json({ message: err });
